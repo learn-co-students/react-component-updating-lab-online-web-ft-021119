7948fb5d9b7a67e9aecc45c22789f4279082fd37
Lizzie <lchan217@gmail.com>
2019-06-24 10:37:44 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -14,7 +14,18 @@ class Timer extends Component {
 
 
   //Your code here
+  // numbers change color every time it increases
+  componentDidUpdate(){
+    this.timer.current.style.color =
+    "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
 
+  shouldComponentUpdate(nextProps, nextState) {
+  if (this.state.time === nextState.time) {
+    return false
+  }
+  return true
+}
 
 
   componentDidMount() {
